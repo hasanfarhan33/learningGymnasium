bec15b9a012dde84236fc10c39b9ae3394d215f1
Farhan Hasan <hasanfarhan33@gmail.com>
2024-01-22 17:37:53 +0000

    maybe final commit. I still don't understand much

--- learningGymnasium.py
+++ learningGymnasium.py
@@ -1,20 +1,72 @@
 import gymnasium as gym 
 import numpy as np
+import matplotlib.pyplot as plt 
+import pickle 
 
 
+def run(episodes, is_training = True, render = False ):
+    env = gym.make("FrozenLake-v1", map_name = "8x8", is_slippery = True, render_mode = "human" if render else None)
+    
+    if is_training: 
+        q = np.zeros((env.observation_space.n, env.action_space.n))
+    else:
+        f = open('frozen_lake8x8.pkl', 'rb')
+        q = pickle.load(f)
+        f.close()
+    
+    learning_rate_a = 0.9
+    discount_factor_g = 0.9 
+    
+    epsilon = 1 
+    epsilon_decay_rate = 0.0001
+    rng = np.random.default_rng() 
+    
+    rewards_per_episode = np.zeros(episodes)
+    
+    for i in range(episodes): 
+        state = env.reset()[0]
+        terminated = False 
+        truncated = False 
+         
+        while(not terminated and not truncated): 
+            if rng.random() < epsilon:
+                action = env.action_space.sample() 
+            else:
+                action = np.argmax(q[state, :])
+                
+            new_state, reward, terminated, truncated, _ = env.step(action)
+            
+            if is_training: 
+                q[state, action] = q[state, action] + learning_rate_a * (reward + discount_factor_g * np.max(q[new_state, :]) - q[state, action])
+            
+            state = new_state 
+            
+        epsilon = max(epsilon - epsilon_decay_rate, 0)
+        
+        if epsilon == 0: 
+            learning_rate_a = 0.0001 
+        
+        if reward == 1: 
+            rewards_per_episode[i] == 1 
+    env.close() 
+    
+    sum_rewards = np.zeros(episodes)
+    
+    for t in range(episodes): 
+        sum_rewards[t] = np.sum(rewards_per_episode[max(0, t-100):(t + 1)])
+    
+    plt.plot(sum_rewards)
+    plt.savefig('frozen_lake8x8.png')
+    
+    if is_training: 
+        f = open("frozen_lake8x8.pkl", "wb")
+        pickle.dump(q, f)
+        f.close() 
+    
 
-env = gym.make('FrozenLake-v1', map_name = "8x8", is_slippery = True, render_mode = "human")
-
-state = env.reset()[0] # States 0 - 63, 0 -> top left, 63 -> bottom right 
-terminated = False  # True when fall in hole or reached goal  
-truncated = False   # True when actions > 200
-
-while (not terminated and not truncated): 
-    action = env.action_space.sample() # actions: left -> 0, down -> 1, right -> 2, up -> 3
-    new_state, reward, terminated, truncated, _ = env.step(action)
-    state = new_state 
-
-env.close() 
-
+if __name__ == '__main__':
+    # run(15000)
+    run(15000, is_training=True, render = False)
+                
  
 
